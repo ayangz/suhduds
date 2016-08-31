8dab0b700c3305492c054dc85df45d02325d71da
Anton Yang <Anton Yang>
2016-08-31 12:40:51 -0400

    more testing

--- form_handler.js
+++ form_handler.js
@@ -1,11 +1,9 @@
+document.getElementById("submit").onclick = function() {
+	var category = document.getElementById("category").value;
+	var img_code = document.getElementById("img_code").value;
+	var size = document.getElementById("size").value;
 
-	document.getElementById("submit").onclick = function() {
-		var category = document.getElementById("category");
-		var img_code = document.getElementById("img_code");
-		var size = document.getElementById("size");
-		alert("suh");
-
-		chrome.storage.sync.set({'category': category, 'img_code': img_code, 'size': size}, function(){
-			alert("fuck yeah");
-		});
-	}
+	chrome.storage.sync.set({'category': category, 'img_code': img_code, 'size': size}, function(){
+		alert(category + img_code + size);
+	});
+}
